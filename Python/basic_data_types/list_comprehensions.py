58dda79fe2e41fac65178ef3b3dc5bc8e807aee2
miguel <miguel.almn@gmail.com>
2021-08-22 17:09:00 -0400

    Python basic data types

---
+++ Python/basic_data_types/list_comprehensions.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    print([[x_, y_, z_] for x_ in range(x + 1) for y_ in range(y + 1) for z_ in range(z + 1) if (x_ + y_ + z_ != n)])
--- Python/introduction/write_a_function.py
+++ Python/introduction/write_a_function.py
@@ -2,6 +2,9 @@ def is_leap(year):
     leap = False
 
     # Write your logic here
+    if year % 4 == 0:
+        if not (year % 100 == 0 and year % 400 != 0):
+            leap = True
 
     return leap
 
