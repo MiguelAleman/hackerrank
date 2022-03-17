87ccb9173e87e5b1a09c08ba4e5094698b50f8a3
miguel <miguel.almn@gmail.com>
2022-03-17 11:42:23 -0400

    Adding another solution for string formatting problem.

--- Python/python_domain/strings/string_formatting.py
+++ Python/python_domain/strings/string_formatting.py
@@ -1,4 +1,7 @@
 def print_formatted(number):
+    """
+    Using built-in functions
+    """
     width = None
     lines = []
     for i in range(number, 0, -1):
@@ -12,6 +15,30 @@ def print_formatted(number):
     print('\n'.join(lines[::-1]))
 
 
+def print_formatted_2(number):
+    """
+    Using string formatting
+    """
+    width = len('{:b}'.format(number))
+    print(
+        '\n'.join(
+            [
+                ' '.join(
+                    [
+                        '{:>{width}d}'.format(x, width=width),
+                        '{:>{width}o}'.format(x, width=width),
+                        '{:>{width}X}'.format(x, width=width),
+                        '{:>{width}b}'.format(x, width=width),
+                    ]
+                )
+                for x in range(1, number + 1)
+            ]
+        )
+    )
+
+
 if __name__ == '__main__':
     n = int(input())
     print_formatted(n)
+    print('\n')
+    print_formatted_2(n)
